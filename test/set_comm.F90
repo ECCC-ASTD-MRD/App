d219385f79f407b3025b824b7d181c6aad20a979
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@ec.gc.ca>
2025-10-23 19:55:18 +0000

    Added App_LogAllRanks test

--- test/set_comm.F90
+++ test/set_comm.F90
@@ -12,10 +12,14 @@ program set_comm
     call MPI_INIT(ierror)
     call App_Start()
 
+    call MPI_Barrier(MPI_COMM_WORLD,ierror);
     call app_log(APP_INFO,'setting communicator')
     comm=MPI_COMM_WORLD
     call app_setmpicomm(comm)
- 
+
+    call app_log(APP_INFO,'log to from default ranks')
+    call app_logallranks(APP_INFO,'log to all ranks')
+  
     app_status=app_end(0)
     call MPI_Finalize(ierror)
 
