3f5402f820afabfe6f2ee0a887d1cc1f2689b62c
Samuel Gilbert <samuel.gilbert@ec.gc.ca>
2026-06-16 15:59:32 +0000

    [tests] Fix indentation

--- test/app_alarm.F90
+++ test/app_alarm.F90
@@ -1,18 +1,18 @@
 program test_app_fortran
+    use app
 
-      use app
+    implicit none
 
-      implicit none
-      integer :: ier
+    integer :: ier
 
-      call app_logstream('stdout')
-      app_ptr = App_Init(APP_MASTER, "app_alarm", "test", "alarm signal test", "now")
-      call App_Start()
+    call app_logstream('stdout')
+    app_ptr = App_Init(APP_MASTER, "app_alarm", "test", "alarm signal test", "now")
+    call App_Start()
 
-      ier=app_alarm(1)
-      call sleep(20)
-   
-      call app_logstats('FORTRAN')
-  
-      app_status=app_end(0)
+    ier = app_alarm(1)
+    call sleep(20)
+
+    call app_logstats('FORTRAN')
+
+    app_status = app_end(0)
 end
